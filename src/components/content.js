ed8c4e9287e7973f368fc0e2677002f491f4f39d
morganbarlow <morgan.barlow2232@gmail.com>
2021-03-27 21:59:36 -0600

    adding color states

--- src/components/content.js
+++ src/components/content.js
@@ -1,13 +1,26 @@
+import randomColor from 'randomcolor';
 import React, { Component } from 'react';
 
 export default class Content extends Component {
+    constructor() {
+        super() 
+
+        this.state = {
+            bGColor : randomColor(),
+            textColor : randomColor()
+        }
+    }
   
     render() {
         return (
             <div className="content-container">
-                <div className= "color-block"/>
-                    Icons are difficult
-                
+                <div className="color-block" 
+                style={{backgroundColor: this.state.bGColor}}>
+                    <div className="color-statement" 
+                    style = {{color : this.state.textColor}}>
+                        <h1>React Icons Are Difficult</h1>
+                    </div>
+                </div>
           
                 <div className="color-button">
                     <button>Click Me</button>
